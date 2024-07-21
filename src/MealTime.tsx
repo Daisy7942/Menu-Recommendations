d49735c3137f9b03fd90ee518f77098ec12245e6
Daisy7942 <zotazoang@gmail.com>
2024-07-21 18:13:53 +0900

    fonts

--- src/MealTime.tsx
+++ src/MealTime.tsx
@@ -24,11 +24,20 @@ const MealTime: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="text-4xl font-bold text-gray-800">
-        {meal} 고민될땐? 
+    <div className='justify-center items-center pt-80'>
+      <div className="relative flex items-center justify-center">
+        <div className="flex-grow border-t border-black" style={{ maxWidth: '100px' }}></div>
+        <span className="px-4">전국민의 고민</span><br/>
+        <div className="flex-grow border-t border-black" style={{ maxWidth: '100px' }}></div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-4xl font-bold text-purple-700">
+          {meal}메뉴 고민될땐? 
+        </div>
       </div>
     </div>
+    
+  
   );
 };
 
